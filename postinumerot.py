01bc697301d6c05d3b390f1898a1209ec0a3db1b
Miika Juhala <77334107+miikajuhala@users.noreply.github.com>
2022-02-05 13:47:02 +0200

    done

--- postinumerot.py
+++ postinumerot.py
@@ -1,1 +1,45 @@
 # Kopioi aikaisempi ratkaisusi tänne. Lisää tarvittaessa myös muut ratkaisusi tiedostot.
+# Esimerkkisuoritus:
+#
+# Kirjoita postitoimipaikka: Porvoo
+# Postinumerot: 06100, 06101, 06150, 06151, 06200, 06400, 06401, 06450, 06500
+
+import math
+import requests
+import json
+
+
+def get_numbers():
+    response_API = requests.get('https://raw.githubusercontent.com/theikkila/postinumerot/master/postcode_map_light.json')
+    #print(response_API.status_code)
+    data = response_API.text
+    realdata = json.loads(data)
+    data1=[]
+
+    postitoimipaikka = "HELSINKI";
+
+    if postitoimipaikka not in realdata.values(): 
+        print("Tuntematon postitoimipaikka")
+    else:
+        for postinumero, kaupunki in realdata.items():
+            if postitoimipaikka==kaupunki:
+                data1.append(postinumero)
+
+    data1.sort()   
+    return print("Postinumerot: "), print(*data1, sep=", ")
+    
+         
+def get_one(postitoimipaikka):
+    response_API = requests.get('https://raw.githubusercontent.com/theikkila/postinumerot/master/postcode_map_light.json')
+    data = response_API.text
+    realdata = json.loads(data)
+    data1=[]
+    
+    if postitoimipaikka not in realdata.values(): 
+        return ("Tuntematon postitoimipaikka")
+    else:
+        for postinumero, kaupunki in realdata.items():
+            if postitoimipaikka==kaupunki:
+                data1.append(postinumero)
+                return data1
+
--- postinumerot_test.py
+++ postinumerot_test.py
@@ -1,1 +1,20 @@
 # Kirjoita postinumerot-moduulin testit tähän tiedostoon
+from postinumerot import get_numbers
+from postinumerot import get_one
+
+
+
+def test_get_numbers_multiple():
+     monta = get_numbers()
+
+     assert len(monta) >= 2 
+
+def test_get_numbers_one():
+    monta = get_one("ESPOO")
+
+    assert len(monta) == 1 
+
+def test_get_false_place():
+    false = get_one("koirakissa")
+
+    assert false=="Tuntematon postitoimipaikka"
